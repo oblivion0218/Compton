839a2399401bda36eea647fed39cf763dafc2943
oblivion0218 <bossi.ricky@gmail.com>
2024-11-07 20:47:30 +0100

    Create Programma semplice in Python.py

---
+++ Programma semplice in Python.py
@@ -0,0 +1,7 @@
+# Programma semplice in Python
+
+# Chiediamo all'utente di inserire il suo nome
+nome = input("Ciao! Come ti chiami? ")
+
+# Stampiamo un messaggio di saluto
+print("Ciao, " + nome + "! Piacere di conoscerti!")
